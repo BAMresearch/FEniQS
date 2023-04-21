bf159af80ba530b10a0ce82bfa30459ebafd2f9a
ajafari <ajafari@ws8232.zit.bam.de>
2023-04-21 16:46:07 +0200

    Add examples of fem

---
+++ examples/fem/models_QS/QSM_GDM_kozicki2013.py
@@ -0,0 +1,132 @@
+import sys
+if './' not in sys.path:
+    sys.path.append('./')
+
+from feniQS.problem.QSM_GDM import *
+from examples.fem.structures.struct_kozicki2013 import *
+
+pth_QSM_GDM_kozicki2013 = CollectPaths('QSM_GDM_kozicki2013.py')
+pth_QSM_GDM_kozicki2013.add_script(pth_QSM_GDM)
+pth_QSM_GDM_kozicki2013.add_script(pth_struct_kozicki2013)
+
+def revise_pars_gdm_kozicki2013(pars):
+    pars.constraint = 'PLANE_STRAIN'
+    pars.E_min = 40e3
+    pars.E = 20e3 # E_total = 60e3
+    pars.e0_min = 6e-4
+    pars.e0 = 5e-4 # e0_total = 11e4
+    pars.ef = 35e-4
+    pars.c_min = 20
+    pars.c = 20 # c_total = 40
+    
+    # pars.shF_degree_u = 2
+    # pars.shF_degree_ebar = 1
+    # pars.integ_degree = 3
+
+def pp_qsm_gdm_kozicki2013(model, solve_options):
+    model.pps[0].plot_residual_at_free_DOFs('Residual-norm of free DOFs', full_file_name=model._path + 'residual_norm_free_DOFs.png')
+    
+    ## Plot CMOD (crack mouth opening displacement)
+    p1 = np.array([0.5 * (model.pars.lx - model.pars.l_notch), 0.0])
+    p2 = np.array([0.5 * (model.pars.lx + model.pars.l_notch), 0.0])
+    p3 = np.array([0.5 * (model.pars.lx - model.pars.l_notch), model.pars.h_notch])
+    p4 = np.array([0.5 * (model.pars.lx + model.pars.l_notch), model.pars.h_notch])
+    ups = model.pps[0].eval_checked_u([p1, p2, p3, p4])
+    cmods_bot = [0] + [us[1,0]-us[0,0] for us in ups]
+    cmods_top = [0] + [us[3,0]-us[2,0] for us in ups]
+    loaded_us_checked = [0] + evaluate_expression_of_t(model.time_varying_loadings['y_middle'], ts=model.pps[0].checkpoints)[1]
+    fig = plt.figure()
+    plt.plot(loaded_us_checked, cmods_bot, marker='<', label='bottom')
+    plt.plot(loaded_us_checked, cmods_top, marker='>', label='top')
+    plt.plot([-0.3, -0.3], [0.02, 0.04], marker='X')
+    plt.text(-0.3, 0.02, f"(-0.3, 0.02)", ha='center', va='top')
+    plt.text(-0.3, 0.04, f"(-0.3, 0.04)", ha='center', va='bottom')
+    plt.title('Crack mouth opening displacement')
+    plt.xlabel('Deflection')
+    plt.ylabel('CMOD')
+    plt.legend()
+    plt.savefig(model._path + 'cmod_vs_us.png', bbox_inches='tight', dpi=300)
+    plt.show()
+    
+    if len(model.pps[0].reaction_dofs) > 0:
+        ## versus time
+        factor = -1e-3 # is only for getting positive values in kN in plots
+        tits = []; _names = []
+        for ii, rp in enumerate(solve_options.reaction_places):
+            tits.append(f"Reaction force at {rp}")
+            _names.append(model._path + f"reaction_force_{ii+1}.png")
+        fss = model.pps[0].plot_reaction_forces(tits, factor=factor \
+                                          , full_file_names=_names)
+        fs = fss[0]
+        ## versus deflection
+        loaded_us = evaluate_expression_of_t(model.time_varying_loadings['y_middle'], ts=model.pps[0].ts)[1]
+        fig = plt.figure()
+        plt.plot(loaded_us, fs, marker='*')
+        _ys = np.linspace(min(fs), max(fs), 100)
+        plt.plot(len(_ys) * [-0.3], _ys, linestyle='dashed')
+        plt.text(-0.3, 0, "$u=-0.3$", ha='left', va='bottom')
+        plt.title('Load-displacement')
+        plt.xlabel('Deflection (mm)')
+        plt.ylabel('f ($kN$)')
+        plt.savefig(model._path + 'load_displacement.png', bbox_inches='tight', dpi=300)
+        
+        ## versus CMOD
+        fs_checked = [fs[i] for i, a in enumerate(loaded_us) if a in loaded_us_checked]
+        cmods_ave = [0.5*(a+cmods_top[i]) for i,a in enumerate(cmods_bot)]
+        fig = plt.figure()
+        plt.plot(cmods_ave, fs_checked, marker='+', label='vs. average CMOD')
+        plt.plot(cmods_top, fs_checked, marker='x', label='vs. top CMOD')
+        _ys = np.linspace(min(fs_checked), max(fs_checked), 100)
+        plt.plot(len(_ys) * [0.02], _ys, linestyle='dashed')
+        plt.text(0.02, 0, "0.02", ha='left', va='bottom')
+        plt.plot(len(_ys) * [0.04], _ys, linestyle='dashed')
+        plt.text(0.04, 0, "0.04", ha='left', va='bottom')
+        plt.title('Load vs. CMOD')
+        plt.xlim((0, 0.2))
+        plt.ylim((0, 2))
+        plt.xlabel('CMOD')
+        plt.ylabel('f ($kN$)')
+        plt.legend()
+        plt.savefig(model._path + 'fs_vs_cmod.png', bbox_inches='tight', dpi=300)
+        
+        plt.show()
+    
+    df.list_timings(df.TimingClear.keep, [df.TimingType.wall])
+    return fss
+    
+if __name__ == "__main__":
+    df.set_log_level(30)
+    
+    ## PARAMETERs
+    pars_struct = ParsKozicki2013()
+    # pars_struct.l_notch = 1. # No notch
+    pars_struct.loading_level *= 2. # larger loading, causing more damage
+    pars_gdm = GDMPars(pars0=pars_struct) # We merge pars_struct to gdm model pars (through pars0)
+    revise_pars_gdm_kozicki2013(pars_gdm)
+    
+    ## MODEL (quasi static)
+    model = get_QSM_GDM(pars_struct=pars_struct, cls_struct=Kozicki2013, pars_gdm=pars_gdm)
+    
+    ## SOLVE OPTIONs
+    solver_options = get_fenicsSolverOptions() # regarding a single load-step
+    solver_options['tol_abs'] = 1e-11
+    solver_options['tol_rel'] = 1e-13
+    # solver_options['type'] = 'snes'
+    # solver_options['lin_sol'] = 'iterative'
+    solve_options = QuasiStaticSolveOptions(solver_options) # regarding incremental solution
+    solve_options.checkpoints = [float(a) for a in (np.arange(1e-1, 1.0, 1e-1))] # this "float" is to be able to properly write "solve_options" into yamlDict file
+    solve_options.t_end = model.pars.loading_t_end
+    solve_options.dt = 0.01
+    solve_options.reaction_places = ['y_middle', 'y_left', 'y_right']
+    
+    ## SOLVE
+    model.solve(solve_options) # 'build_solver' is included there.
+    # Write parameters to yaml files
+    model.yamlDump_pars()
+    solve_options.yamlDump_toDict(_path=model._path)
+    
+    ## POST-PROCESS
+    fss = pp_qsm_gdm_kozicki2013(model, solve_options)
+    
+    df.set_log_level(20)
+    
---
+++ examples/fem/structures/struct_kozicki2013.py
@@ -0,0 +1,81 @@
+import sys
+if './' not in sys.path:
+    sys.path.append('./')
+
+from feniQS.structure.struct_bend3point2d import *
+
+pth_struct_kozicki2013 = CollectPaths('struct_kozicki2013.py')
+pth_struct_kozicki2013.add_script(pth_struct_bend3point2d)
+
+class ParsKozicki2013(ParsBend3Point2D):
+    """
+    The geometry/mesh/loading for the three-point bending experiment presented in:
+        https://link.springer.com/content/pdf/10.1007/s11340-013-9781-y.pdf
+    """
+    def __init__(self, **kwargs):
+        if len(kwargs)==0: # Some values are set
+            ## GEOMETRY
+            self.lx = 320.
+            self.ly = 80.
+            self.l_notch = 3. # width of notch
+            self.h_notch = 8. # hight of notch
+            self.left_notch = (self.lx - self.l_notch) / 2
+            self.left_sup = (self.lx - 240.) / 2. + 0.
+            self.right_sup = self.lx - self.left_sup
+            self.right_sup_w = 0.
+            # self.right_sup_w = (self.left_sup - 0.) / 1.
+            self.left_sup_w = self.right_sup_w
+            load_span_scale = 0.014 # of total beam length
+            self.x_from = (1. - load_span_scale)  * self.lx / 2. # for loading
+            self.x_to = (1. + load_span_scale)  * self.lx / 2.
+            self.cmod_left = self.left_notch
+            self.cmod_right = self.left_notch + self.l_notch
+            
+            ## MESH
+            self.resolutions = {'res_y': 4, 'scale': 0.25, 'embedded_nodes': (), 'refinement_level': 0}
+                # scale (regarding mesh size) is total and towards the center with respect to the left_right sides
+            
+            ## LOADs and BCs
+            self.fix_x_at = 'left'
+            
+            self.loading_control = 'u'
+            self.loading_level = - 0.35
+            # self.loading_level = - 0.10 # Only elastic
+            
+            # self.loading_control = 'f'
+            # self.loading_level = -587.8071109591015 / (load_span_scale * self.lx) # Only elastic
+            
+            self.loading_scales = (1,)
+            self.loading_case = 'ramp'
+            self.loading_N = 1.0 # only relevant if case=sin or case=zigzag
+            self.loading_T = 1.0
+            self.loading_t_end = self.loading_T
+            
+            ## REST
+            self._write_files = True
+            self._plot = True
+            
+        else: # Get from a dictionary
+            ParsBend3Point2D.__init__(self, **kwargs)
+
+class Kozicki2013(Bend3Point2D):
+    """
+    The model of:
+        https://link.springer.com/content/pdf/10.1007/s11340-013-9781-y.pdf
+    """
+    def __init__(self, pars, _path=None, _name=None):
+        if _name is None:
+            _name = 'kozicki2013'
+        Bend3Point2D.__init__(self, pars=pars, _path=_path, _name=_name)
+
+if __name__ == "__main__":
+    pars = ParsKozicki2013()
+    embedded_nodes = ParsKozicki2013.get_regular_grid_points(pars, include_edges=False)
+    ParsBend3Point2D.set_embedded_nodes(pars, embedded_nodes)
+    pars.resolutions['scale'] = 0.2
+    pars.resolutions['res_y'] = 5
+    _name = None if len(pars.resolutions['embedded_nodes'])==0 else f"kozicki2013_embedded_{len(pars.resolutions['embedded_nodes'])}"
+    struct = Kozicki2013(pars, _name=_name)
+    struct.yamlDump_pars()
+    
+    print(f"Num. of (cells, vertices) = ({struct.mesh.num_cells()}, {struct.mesh.num_vertices()}) .")
