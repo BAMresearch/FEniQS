0b1ab1611fc90a2aabe0c9e0a40092c2031c0a8b
ajafari <ajafari@VDI0039.zit.bam.de>
2024-11-06 11:32:40 +0100

    Extend example of elemental forces to several elements

--- illustrate/elemental_assembly_of_stress_2D.py
+++ illustrate/elemental_assembly_of_stress_2D.py
@@ -1,7 +1,7 @@
 from feniQS.structure.helper_mesh_fenics import *
 import matplotlib.pyplot as plt
 
-def visualize_projected_stress(q_ss, mesh, DG_degree):
+def visualize_projected_stress(q_ss, mesh, DG_degree=1):
     ### PROJECT STRESS on DG-space ###
     elem_ss_DG = df.VectorElement(family="DG", cell=mesh.ufl_cell(), \
                          degree=DG_degree, dim=ss_dim, quad_scheme="default")
@@ -21,13 +21,17 @@ def visualize_projected_stress(q_ss, mesh, DG_degree):
         plt.show()
 
 if __name__=='__main__':
+    np.random.seed(1983)
     shF_degree = 4 # Concerning virtual displacement
     integ_degree = 3 # Regarding Quadrature space that stores stresses
     ss_dim = 3 # For: sigma_xx, sigma_yy, sigma_xy
+    n_refinemensts = 1
 
     ### MESH ###
     _scale = 1.0 # for geometry
     mesh = one_cell_mesh_2D(0., 0., _scale*20., _scale*10., -_scale*5., _scale*20.) # A mesh with ONE single cell
+    for i in range(n_refinemensts):
+        mesh = df.refine(mesh)
     plt.figure()
     df.plot(mesh)
     plt.title("A single element")
@@ -39,18 +43,10 @@ if __name__=='__main__':
                             , degree=integ_degree, dim=ss_dim, quad_scheme="default")
     i_ss = df.FunctionSpace(mesh, elem_ss)
     q_ss = df.Function(i_ss)
-    if integ_degree==1:
-        q_ss.vector()[:] = [125, -52, 632]
-    elif integ_degree==2:
-        q_ss.vector()[:] = [125, -52, 632, 1036, -899, 21, 95, -995, 11]
-    elif integ_degree==3:
-        q_ss.vector()[:] = [125, -52, 632, 1036, -899, 21, 95, -995, 11] \
-                        + [-1025, 0, -540, 13, -228, 63, 902, -12995, -1]
-    else:
-        raise NotImplementedError(f"Example supports 'integ_degree' up to 3.")
+    q_ss.vector()[:] = 100. * (2 * np.random.rand(i_ss.dim()) - 1.) # some random stress field
 
     ### VISUALIZATION (of stresses) ??? ###
-    visualize_projected_stress(q_ss, mesh, DG_degree=integ_degree)
+    visualize_projected_stress(q_ss, mesh)
 
     ### NODAL INTERNAL FORCEs (entire vector) ###
     elem_u = df.VectorElement(family='CG', cell=mesh.ufl_cell() \
@@ -63,40 +59,48 @@ if __name__=='__main__':
     metadata = {"quadrature_degree": integ_degree, "quadrature_scheme": "default"}
     dxm = df.dx(domain=mesh, metadata=metadata)
     f_int = df.inner(eps(u_), q_ss) * dxm # internal forces
-    f_int_local = df.assemble_local(f_int, df.Cell(mesh, 0))
 
-    ### EXTRACT f_int_x, f_int_y ###
-    dofs_cell = list(i_u.dofmap().cell_dofs(0))
     dofs_x = i_u.sub(0).dofmap().dofs()
     dofs_y = i_u.sub(1).dofmap().dofs()
-    ids_x = [dofs_cell.index(i) for i in dofs_x]
-    ids_y = [dofs_cell.index(i) for i in dofs_y]
-    f_int_x = f_int_local[ids_x]
-    f_int_y = f_int_local[ids_y]
-
-    ### COMPUTE ROTATIONAL MOMENTUM ###
     cs = i_u.tabulate_dof_coordinates()
-    cs_x = cs[dofs_x, :]
-    cs_y = cs[dofs_y, :]
-    rot = 0. # rotational momentum
-    for f, r in zip(f_int_x, cs_x):
-        rot += - f * r[1] # clockwise
-    for f, r in zip(f_int_y, cs_y):
-        rot += f * r[0] # counter-clockwise
+    for i_cell in range(mesh.num_cells()):
+        ### EXTRACT f_int_x, f_int_y ###
+        f_int_local = df.assemble_local(f_int, df.Cell(mesh, i_cell))
+        dofs_cell = list(i_u.dofmap().cell_dofs(i_cell))
+        ids_x = [dofs_cell.index(dof) for dof in dofs_x if (dof in dofs_cell)]
+        ids_y = [dofs_cell.index(dof) for dof in dofs_y if (dof in dofs_cell)]
+        f_int_x = f_int_local[ids_x]
+        f_int_y = f_int_local[ids_y]
 
-    ### PRINTs ###
-    max_force = np.max(abs(f_int_local))
-    print(f"Local internal forces: {f_int_local}.\nMaximum absolute value = {max_force:.2e}.\n")
-    print(f"Sum of forces (fx, fy) = ({sum(f_int_x):.1e}, {sum(f_int_y):.1e})")
-    print(f"Rotational momentum = {rot:.1e}")
+        ### COMPUTE ROTATIONAL MOMENTUM ###
+        cs_x = np.array([cs[dofs_cell[i], :] for i in ids_x])
+        cs_y = np.array([cs[dofs_cell[i], :] for i in ids_y])
+        plt.figure()
+        df.plot(mesh)
+        plt.plot(cs_x[:,0], cs_x[:,1], marker='.', linestyle='')
+        plt.title(f"Mesh and nodes of cell-{i_cell}.")
+        plt.show()
+        rot = 0. # rotational momentum
+        for f, r in zip(f_int_x, cs_x):
+            rot += - f * r[1] # clockwise
+        for f, r in zip(f_int_y, cs_y):
+            rot += f * r[0] # counter-clockwise
 
-    ### CHECK STATIC BALANCE ###
-    tol_f = max_force * 1e-14 # relative to the maximum force
-        # Translational
-    assert abs(sum(f_int_x)) < tol_f
-    assert abs(sum(f_int_y)) < tol_f
-    tol_r = max_force * mesh.hmax() * 1e-14
-        # Rotational
-    assert abs(rot) < tol_r
+        ### PRINTs ###
+        max_force = np.max(abs(f_int_local))
+        print(f"# Local internal forces: {f_int_local}.\n# Maximum absolute value = {max_force:.2e}.\n")
+        print(f"## Sum of forces (fx, fy) = ({sum(f_int_x):.1e}, {sum(f_int_y):.1e})")
+        print(f"## Rotational momentum = {rot:.1e}")
+
+        ### CHECK STATIC BALANCE ###
+        tol_f = max_force * 1e-14 # relative to the maximum force
+            # Translational
+        assert abs(sum(f_int_x)) < tol_f
+        assert abs(sum(f_int_y)) < tol_f
+        tol_r = max_force * mesh.hmax() * 1e-14
+            # Rotational
+        assert abs(rot) < tol_r
+
+        print(f"\n### Verified static balance among 'elemental internal forces' of cell-{i_cell}.\n")
 
     print("\n----- DONE! -----")
